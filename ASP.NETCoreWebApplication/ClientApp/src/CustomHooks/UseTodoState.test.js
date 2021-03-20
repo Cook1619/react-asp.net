87ae88d87984ee84dac58573d47b01602d8bce7f
Mathew Cook <cook6348@gmail.com>
2021-03-20 09:27:32 -0500

    Adding custom hooks

---
+++ ASP.NETCoreWebApplication/ClientApp/src/CustomHooks/UseTodoState.test.js
@@ -0,0 +1,27 @@
+﻿import { useState } from "react";
+import useTodoState from "./UseTodoState";
+
+jest.mock("react");
+
+describe("useTodoState", () => {
+    test("makes sure addtodo works", () => {
+        const todos = [
+            { id: "123abc", task: "This is a new todo", completed: true },
+        ];
+        const setTodos = jest.fn();
+        useState.mockReturnValue([todos, setTodos]);
+        const { addTodo } = useTodoState();
+        addTodo();
+        expect(setTodos).toHaveBeenCalled();
+    });
+    test("should edit a todo", () => {
+        const todos = [
+            { id: "123abc", task: "This is a new todo", completed: true },
+        ];
+        const setTodos = jest.fn();
+        useState.mockReturnValue([todos, setTodos]);
+        const { editTodo } = useTodoState();
+        editTodo();
+        expect(setTodos).toHaveBeenCalled();
+    });
+});
---
+++ ASP.NETCoreWebApplication/ClientApp/src/CustomHooks/useToggle.js
@@ -0,0 +1,13 @@
+﻿import { useState } from "react";
+
+const useToggle = (initalVal = false) => {
+    //call useState
+    const [state, setState] = useState(initalVal);
+    const toggle = () => {
+        setState(!state);
+    };
+    //return piece of state and the function which toggles it
+    return [state, toggle];
+};
+
+export default useToggle;
